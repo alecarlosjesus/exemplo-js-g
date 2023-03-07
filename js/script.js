6ca89fd301c41305f603e5f3c0c8383dbf1b3305
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-03-07 11:49:13 -0300

    script.js adicionado ao html.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Este é um comentário de linha
+
+/**
+ * É um comentário
+ * de
+ * Bloco
+ */
+
+//Exemplo das funções do console.
+console.log("Olá mundo!")
+console.error("Bom dia!")
+console.warn("Será?")
+console.info("Vai ser...")
